1d5e619f4c842f785aef89e28fcca09ca39875a0
juan.cardona <felipe7u7432@gmail.com>
2023-05-14 17:34:55 -0500

    ejemplo y codigo imperativo

--- button-whit-js.js
+++ button-whit-js.js
@@ -1,15 +1,19 @@
 
 // Con Js Vanilla lo comun es cambiar el codigo para que el funcionamineto funcione para todos.
-const button = document.getElementById("me-gusta");
 
-button.addEventListener('click', function() {
 
-    if(button.classList.contains('liked')){
-        button.classList.remove('liked');
-        button.innerText = 'No me gusta😿';
-    } else {
-        button.classList.add('liked');
-        button.innerText = 'Me gusta❤️';
-    }
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
 
-});
+        if(button.classList.contains('liked')){
+            button.classList.remove('liked');
+            button.innerText = 'No me gusta😿';
+        } else {
+            button.classList.add('liked');
+            button.innerText = 'Me gusta❤️';
+        }
+    
+    });
+})
+
