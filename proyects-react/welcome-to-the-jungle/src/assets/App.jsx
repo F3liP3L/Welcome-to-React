9e71c7af8a3f6a136f3dcb303e7443e4e32c30cf
juan.cardona <felipe7u7432@gmail.com>
2023-05-19 17:16:55 -0500

    agregando imagenes mas dinamicas

--- proyects-react/welcome-to-the-jungle/src/assets/App.jsx
+++ proyects-react/welcome-to-the-jungle/src/assets/App.jsx
@@ -3,10 +3,11 @@ import './index.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 export function App (){
     return(
-        <>
-            <TwitterFollowCard userName="javalover777" name="Java Lover" urlImage="https://res.cloudinary.com/teepublic/image/private/s--4M3PnU_p--/c_fit,g_north_west,h_920,w_671/co_ffffff,e_outline:41/co_ffffff,e_outline:inner_fill:1/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/t_watermark_lock/c_limit,f_jpg,h_630,q_90,w_630/v1607436695/production/designs/17067387_0.jpg" />
-            <TwitterFollowCard userName="johnxhina" name="John China" urlImage="https://preview.redd.it/my4q5oe1xhz81.jpg?width=640&crop=smart&auto=webp&s=6714d10852bbea9b09bd187097ed9efee0488274" />
-
-        </>
+        <div className='container-tw'>
+            <TwitterFollowCard userName="java" name="Java Lover" />
+            <TwitterFollowCard userName="john" name="John China" />
+            <TwitterFollowCard userName="f3lip3l" name="Felipe Lora" />
+            <TwitterFollowCard userName="deynox1" name= "Pablito" />
+        </div>
     );
 }
--- proyects-react/welcome-to-the-jungle/src/assets/TwitterFollowCard.jsx
+++ proyects-react/welcome-to-the-jungle/src/assets/TwitterFollowCard.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import './twitterFollowCard.css'
 // eslint-disable-next-line react/prop-types
-export function TwitterFollowCard ({userName, name, isFollowing, urlImage}){
+export function TwitterFollowCard ({userName, name, isFollowing}){
     return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' alt="image" src={urlImage}/>
+                <img className='tw-followCard-avatar' alt="image" src= {`https://unavatar.io/${userName}`}/>
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>{userName}</span>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
             <aside>
